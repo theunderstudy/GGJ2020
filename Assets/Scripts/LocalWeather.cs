1ea1ee7eb2f10a6f25139ac884e148d81ceadacf
Frankie <f.joseph.stephens@gmail.com>
2020-01-31 01:04:48 +1300

    Local weather now grabs city from IP

--- Assets/Scripts/LocalWeather.cs
+++ Assets/Scripts/LocalWeather.cs
@@ -10,19 +10,32 @@ public class LocalWeather : MonoBehaviour
     }
     IEnumerator GetWeather()
     {
-        // build location text
-        string location = "wellington";
-        WWW request = new WWW("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=af38440a4558fb5178b658ed3bbddfb0");
-        // WWW request = new WWW("http://api.openweathermap.org/data/2.5/weather?q=Anuradhapura,Sri Lanka&APPID=e3a642cec13d52496490dfa8e9ba11d3");
-        yield return request;
 
-        if (request.error == null)
+
+        WWW _cityRequest = new WWW("https://api.ipdata.co/city?api-key=9b458d7bc15d0035c5564696960d4fedea2533b290e9383f69b06f10");
+        yield return _cityRequest;
+
+        if (_cityRequest.error == null)
         {
-            Debug.Log(request.text);
+           
         }
         else
         {
-            Debug.Log(request.error);
+            Debug.Log("City get failed" + _cityRequest.error);
+            yield break;
+        }
+
+
+        string _location = _cityRequest.text    ;
+        WWW _weatherRequest = new WWW("http://api.openweathermap.org/data/2.5/weather?q=" + _location + "&APPID=af38440a4558fb5178b658ed3bbddfb0");
+        yield return _weatherRequest;
+        if (_weatherRequest.error == null)
+        {
+            Debug.Log(_weatherRequest.text);
+        }
+        else
+        {
+            Debug.Log("Weather get failed " +_weatherRequest.error);
         }
     }
 }
