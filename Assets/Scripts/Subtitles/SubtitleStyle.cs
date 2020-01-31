f68e8dca08384ea1d0204cd0f3c136a1c2394d31
Patty <8854352+pattyqubed@users.noreply.github.com>
2020-01-31 21:47:53 +1300

    A bunch of rendering stuff!
    Added shaders plugins for envrioment and water.
    Added subtitle scripts.

---
+++ Assets/Scripts/Subtitles/SubtitleStyle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SubtitleStyle
+{
+    public Color OutlineColour;
+    public Color NameColour;
+    public Color TextColour;
+    public Font fontSyle;
+    public int SubtitleSize;
+    public float OutLineSize;
+
+    public SubtitleStyle()
+    {
+    }
+
+    /// <summary>
+    /// struct for storing all subtitle style data.
+    /// </summary>
+    /// <param name="outline">Subtutle outline colour</param>
+    /// <param name="name">Character Speaking</param>
+    /// <param name="text">Character Dialog</param>
+    /// <param name="subtitleSize">Text font size</param>
+    /// <param name="outLineSize">Size of the text outline</param>
+    public SubtitleStyle(Color outline, Color name, Color text, Font font, int subtitleSize, float outLineSize)
+    {
+        OutlineColour = outline;
+        fontSyle = font;
+        NameColour = name;
+        TextColour = text;
+        SubtitleSize = subtitleSize;
+        OutLineSize = outLineSize;
+    }
+}
---
+++ Assets/Scripts/Subtitles/UI_SubtitleTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_SubtitleTest : MonoBehaviour
+{
+    public Color speakerName, dialog, outline;
+    public int size = 30;
+    public UI_Subtitle_Controller _Subtitle_Controller;
+    public float textOutlineSize;
+    public Font font;
+
+    private void Awake()
+    {
+        _Subtitle_Controller = FindObjectOfType<UI_Subtitle_Controller>();
+        TestStyle();
+    }
+
+    void TestStyle()
+    {
+        _Subtitle_Controller.SubtitleStyle.fontSyle = font;
+        _Subtitle_Controller.SubtitleStyle.NameColour = speakerName;
+        _Subtitle_Controller.SubtitleStyle.TextColour = dialog;
+        _Subtitle_Controller.SubtitleStyle.OutlineColour = outline;
+        _Subtitle_Controller.SubtitleStyle.SubtitleSize = size;
+        _Subtitle_Controller.SubtitleStyle.OutLineSize = textOutlineSize;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            TestStyle();
+            _Subtitle_Controller.SetSubtitleStyle();
+        }
+    }
+}
