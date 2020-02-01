9e552f42b6e4562e8ed58e703a3e2e76742bb389
aurynn shaw <self@aurynn.com>
2020-02-01 16:20:26 +1300

    Intermediate values (45 degrees) now in movement controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,9 @@ public class PlayerController : MonoBehaviour
     private GameObject playerObject; // do I need to do this?
     private GameObject ground;
     private Dictionary<KeyCode, Vector3> movement;
-    private Dictionary<KeyCode, Vector3> rotationTargets;
+    private Dictionary< HashSet<KeyCode>, Vector3> rotationTargets;
     
     private bool performRotation = false;
-    private Vector3 rotationTarget;
     public float speed = 1.0f;
     public float rotationSpeed = 45.0f; // 45 degrees per tick?
 
@@ -29,31 +28,63 @@ public class PlayerController : MonoBehaviour
         movement.Add(KeyCode.S, Vector3.back);
         movement.Add(KeyCode.D, Vector3.right);
 
-        rotationTargets = new Dictionary<KeyCode, Vector3>();
+        rotationTargets = new Dictionary<HashSet<KeyCode>, Vector3>(HashSet<KeyCode>.CreateSetComparer());
 
-        // Hardcoded because that's easier right now
-        rotationTargets.Add(KeyCode.W, new Vector3(0f,270f,0f));
-        rotationTargets.Add(KeyCode.S, new Vector3(0f,90f,0f));
-        rotationTargets.Add(KeyCode.D, new Vector3(0f,0f,0f));
-        rotationTargets.Add(KeyCode.A, new Vector3(0f,180f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.W }), new Vector3(0f,270f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.S }), new Vector3(0f,90f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.D }), new Vector3(0f,0f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.A }), new Vector3(0f,180f,0f));
+
+        // W+D should be 305f
+        // S+D should be 45f
+        // S+A should be 135f
+        // W+A should be 215f
+
+        rotationTargets.Add(setOf(new[] { KeyCode.W, KeyCode.D }), new Vector3(0f,305f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.S, KeyCode.D }), new Vector3(0f,45f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.S, KeyCode.A }), new Vector3(0f,135f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.W, KeyCode.A }), new Vector3(0f,215f,0f));
+
+        // If you're holding more than 2 keys,
+        // I mean on one level you're a monster
+        // But on the other level, we can just figure that out easily enough
+
+        rotationTargets.Add(setOf(new[] { KeyCode.W,KeyCode.A,KeyCode.D }), new Vector3(0f,270f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.S,KeyCode.A,KeyCode.D }), new Vector3(0f,90f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.D, KeyCode.W, KeyCode.S }), new Vector3(0f,0f,0f));
+        rotationTargets.Add(setOf(new[] { KeyCode.A, KeyCode.W, KeyCode.S }), new Vector3(0f,180f,0f));
     }
 
     // Update is called once per frame
     void Update()
     {
+        HashSet<KeyCode> usedKeys = new HashSet<KeyCode>();
+        
         foreach (var k in movement) {
             if (Input.GetKey(k.Key)) {
                 transform.Translate((k.Value * Time.deltaTime) * speed, ground.transform);
-                performRotation = true;
-                rotationTargets.TryGetValue(k.Key, out rotationTarget);
-                transform.eulerAngles = rotationTarget;
+                usedKeys.Add(k.Key);
             }
         }
-        // if (performRotation) {
-        //     // currentEulerAngles += new Vector3(0, , z) * Time.deltaTime * rotationSpeed;
-
-        //     //apply the change to the gameObject
-            
-        // }
+        // Okay we have KEYS and A LOOKUP TABLE
+        // WOO
+        
+        if (usedKeys.Count > 0) {
+            Debug.Log("keys used " + usedKeys.Count);
+            Debug.Log(usedKeys);
+            Vector3 rotationTarget;
+            rotationTargets.TryGetValue(usedKeys, out rotationTarget);
+            transform.eulerAngles = rotationTarget;
+            // if (currentEuler != rotationTarget) {
+                
+            // }
+        }
+    }
+    private HashSet<KeyCode> setOf (KeyCode[] keys) {
+        HashSet<KeyCode> keyCodes =  new HashSet<KeyCode>();
+        foreach (KeyCode k in keys) {
+            keyCodes.Add(k);
+        }
+        return keyCodes;
     }
 }
