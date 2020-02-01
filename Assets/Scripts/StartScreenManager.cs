8cb306d93909840f8d3dbbf9306d10a866a057c0
aurynn shaw <self@aurynn.com>
2020-02-02 11:58:01 +1300

    Adds simple start screen prefab

---
+++ Assets/Scripts/StartScreenManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class StartScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject canvas;
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame() {
+        Debug.Log("CLAEK BUOTOMN");
+        canvas.SetActive(false);
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+}
